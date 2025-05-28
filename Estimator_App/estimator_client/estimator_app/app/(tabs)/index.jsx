77f5f74ee844d3fbbdd402c826632a6bd218ab6f
John Bailey Williams <johnbaileywilliams96@gmail.com>
2025-05-28 11:55:53 -0500

    initial commit

---
+++ Estimator_App/estimator_client/estimator_app/app/(tabs)/index.jsx
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Estimator App</Text>
+    </View>
+  )
+}
+
+export default app
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'grey',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+})
